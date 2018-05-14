f7808e45d11a615c258dcefe7a9b1199c1afbc23
Jean Lambert <jeeaan1702@gmail.com>
2018-05-13 23:23:43 -0700

    new folder structure

--- helpers/configs.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    url: 'postgres://postgres:masterkey@127.0.0.1:5432/ShoppingCart',
-}
-module.exports = config;
--- helpers/db.js
+++
@@ -1,5 +0,0 @@
-const config = require('./configs');
-const pgp = require('pg-promise')();
-const db = pgp(config.url);
-
-module.exports = db;
